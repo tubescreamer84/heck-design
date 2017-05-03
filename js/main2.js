822d3ca780b7fe77ecedba4fe719063b22df16ce
Josh Heck <josh@heck.design>
2017-05-02 23:50:47 -0700

    More layout changes 2

---
+++ js/main2.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.scroll-down').click(function(){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top - $('header').outerHeight()
+        }, 1000);
+    });
+
+
+
+});
+
+$(window).scroll(function(){
+   console.log( $(window).scrollTop());
+   if($(window).scrollTop() > 0){
+       $('body').addClass('scrolling');
+   }else{
+       $('body').removeClass('scrolling');
+   }
+
+});
